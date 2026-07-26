64f040f3e509da26623d726e47e775a79ca13a6a
viediet777-hub <viediet777@gmail.com>
2026-07-26 19:48:13 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,627 @@
+BOT_TOKEN = "8620881901:AAHhBwB4R4rKvjGJATDGvnn4lV4UNV2qT44"
+ADMIN_ID = 1364476174
+
+DATABASES = {
+    "aaaa": {
+        "url": "https://aaaa-b3749-default-rtdb.firebaseio.com",
+        "keys": ["AIzaSyANHri0JIWEroUrloP97KGAcIOKMwT4UgU", "AIzaSyD74TOQXfWjYvfwDQ06U38xhdRUIVJ4Afs"]
+    },
+    "access20": {
+        "url": "https://access20-3fc38-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "activity": {
+        "url": "https://activity-e16b3-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "aditya": {
+        "url": "https://aditya-9f66b-default-rtdb.firebaseio.com",
+        "keys": ["AIzaSyAfxZZuDuPSWOFe3OnpD2NfpOsRU37iM0E"]
+    },
+    "ai-rto-9": {
+        "url": "https://ai-rto-9-default-rtdb.firebaseio.com",
+        "keys": ["AIzaSyDXexlXH8-gA37NSc4V8D5nXIHVn2gRAqE"]
+    },
+    "ajay": {
+        "url": "https://ajay-33c1b-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "ajna": {
+        "url": "https://ajna-20fc4-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "alpha": {
+        "url": "https://alpha-af0d2.firebaseio.com",
+        "keys": []
+    },
+    "ankur": {
+        "url": "https://ankur-2511f-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "annapunna": {
+        "url": "https://annapunna-12b79-default-rtdb.firebaseio.com",
+        "keys": ["AIzaSyCgEzi1tlT5gBTr8y9AsrDFtrlRbDJ2D4w"]
+    },
+    "anvith6": {
+        "url": "https://anvith6-9450e-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "apkdriod": {
+        "url": "https://apkdriod-f6fb9-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "apkpure": {
+        "url": "https://apkpure-6eb6a-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "app-2": {
+        "url": "https://app-2-7ac78-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "asdtest": {
+        "url": "https://asdtest-project-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "aya-baby": {
+        "url": "https://aya-baby-c3a6b-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "aya-wed": {
+        "url": "https://aya-wed-anvith-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "babu": {
+        "url": "https://babu-2b2c2-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "bandhan2": {
+        "url": "https://bandhan2-7jan-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "bank-e-kyc": {
+        "url": "https://bank-e-kyc-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "bbbbbb8": {
+        "url": "https://bbbbbb8-a06fb-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "boi-3": {
+        "url": "https://boi-3-8914d-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "bossuun": {
+        "url": "https://bossuun-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "business-apps": {
+        "url": "https://business-apps-5aeb2-default-rtdb.firebaseio.com",
+        "keys": ["AIzaSyCuuRFe_R7Pu65TWetKnZ4LlXHDyGINQMA"]
+    },
+    "business-apps-ba1": {
+        "url": "https://business-apps-ba1-f86b7-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "can-4": {
+        "url": "https://can-4-668a0-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "challan5": {
+        "url": "https://challan5-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "chalo": {
+        "url": "https://chalo-a536a-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "chfjfj": {
+        "url": "https://chfjfj-c2857-default-rtdb.firebaseio.com",
+        "keys": ["AIzaSyCAD1dGu5emRyr5YpnmLvwqffaK78hjAFI"]
+    },
+    "chhnuk05": {
+        "url": "https://chhnuk05-3188e-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "ck-kumar3": {
+        "url": "https://ck-kumar3-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "comeback": {
+        "url": "https://comeback-5b876-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "craxs": {
+        "url": "https://craxs-4c542-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "cs23": {
+        "url": "https://cs23-9e709-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "csforme": {
+        "url": "https://csforme-dc64a-default-rtdb.firebaseio.com",
+        "keys": ["AIzaSyCJE7GmmKKwJSCajCDIxOa8AIQSfycxy6o"]
+    },
+    "customer03support": {
+        "url": "https://customer03support-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "dark": {
+        "url": "https://dark-274b4-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "darknet": {
+        "url": "https://darknet-26b68-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "davil": {
+        "url": "https://davil-d4e77-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "demon-4": {
+        "url": "https://demon-4-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "dhani": {
+        "url": "https://dhani-aa151-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "dogla": {
+        "url": "https://dogla-de225-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "duuu": {
+        "url": "https://duuu-dc41d-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "dwala": {
+        "url": "https://dwala-3d1ff-default-rtdb.firebaseio.com",
+        "keys": [
+            "AIzaSyAW0ODNbYscLvfR8peqI7mEdf_16dArlws",
+            "AIzaSyCOcxmOCivdRSq4w-pYS44g2-P7HdYUyKE",
+            "AIzaSyANVndD8aVFoGpg-w5SbrHPGajrYv5wpzo",
+            "AIzaSyCgEzi1tlT5gBTr8y9AsrDFtrlRbDJ2D4w"
+        ]
+    },
+    "dyno": {
+        "url": "https://dyno-1b564-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "dyydd": {
+        "url": "https://dyydd-c53c8-default-rtdb.firebaseio.com",
+        "keys": ["AIzaSyBIHeqqdiLPEzZ5CxpkSVh8J0jrSDqq9Y4"]
+    },
+    "e5turnament2": {
+        "url": "https://e5turnament2-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "fir-1": {
+        "url": "https://fir-1fa16-default-rtdb.firebaseio.com",
+        "keys": ["AIzaSyCTEGsutg53vY2cdFfxPRdYkniPJWPgwMA"]
+    },
+    "fir-27": {
+        "url": "https://fir-27c9e-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "fir-408f9": {
+        "url": "https://fir-408f9-default-rtdb.firebaseio.com",
+        "keys": ["AIzaSyDHdyW-rPI-77aq4G8I82Z0Sl4wtdFeBX4"]
+    },
+    "fir-e8ad7": {
+        "url": "https://fir-e8ad7-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "fires": {
+        "url": "https://fires-847da-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "flash-v7": {
+        "url": "https://flash-v7powerengine-v7-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "fortydata": {
+        "url": "https://fortydata-fee65-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "fpro3indus": {
+        "url": "https://fpro3indus-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "gaandkiaand": {
+        "url": "https://gaandkiaand-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "gggggg": {
+        "url": "https://gggggg-979bd-default-rtdb.firebaseio.com",
+        "keys": ["AIzaSyAFtNtxRZMX2uBuISXhbCw2SS9n2gh5S4U"]
+    },
+    "gjhghjj": {
+        "url": "https://gjhghjj-3d251-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "go-one": {
+        "url": "https://go-one-1b6b2-default-rtdb.firebaseio.com",
+        "keys": ["AIzaSyD74TOQXfWjYvfwDQ06U38xhdRUIVJ4Afs"]
+    },
+    "gren": {
+        "url": "https://gren-ff2af-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "gsjjshdbs": {
+        "url": "https://gsjjshdbs-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "hdhdhdh": {
+        "url": "https://hdhdhdh-38ae0-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "hdjdjdj": {
+        "url": "https://hdjdjdj-a73f2-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "hdmax1": {
+        "url": "https://hdmax1-58366-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "hopkhfg": {
+        "url": "https://hopkhfg-9981a-default-rtdb.firebaseio.com",
+        "keys": ["AIzaSyAOxY3IOIy4yjB9JqJx8mWMRXX8UqRoHXU"]
+    },
+    "igii": {
+        "url": "https://igii-1d529-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "imdum": {
+        "url": "https://imdum-6e873-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "indus-1": {
+        "url": "https://indus-1-cec4f-default-rtdb.firebaseio.com",
+        "keys": ["AIzaSyANHri0JIWEroUrloP97KGAcIOKMwT4UgU"]
+    },
+    "jamtar7": {
+        "url": "https://jamtar7-95f77-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "jamtara118": {
+        "url": "https://jamtara118-7cd20-default-rtdb.firebaseio.com",
+        "keys": [
+            "AIzaSyDnb1rrYxnrPRFSJ3Y1qwgjrmZgOWBlYbc",
+            "AIzaSyC56Erj8glA0un0ZCJ0_-5kvvKiw2lLuyg"
+        ]
+    },
+    "jamtara140": {
+        "url": "https://jamtara140-73bf7-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "jamtara150": {
+        "url": "https://jamtara150-62b22-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "jamtara181": {
+        "url": "https://jamtara181-default-rtdb.firebaseio.com",
+        "keys": ["AIzaSyCv4JJw_4ruIYnNjwuWqnvmk4FZz1n7F4M"]
+    },
+    "jamtara74": {
+        "url": "https://jamtara74-c231e-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "jayma": {
+        "url": "https://jayma-9ce22-default-rtdb.firebaseio.com",
+        "keys": ["AIzaSyAVwTIwqW_oKyXZhaA6HFnNB7uD4k3U1tY"]
+    },
+    "jsjsjdj": {
+        "url": "https://jsjsjdj-7f0d1-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "kaatil": {
+        "url": "https://kaatil-12346-default-rtdb.firebaseio.com",
+        "keys": ["AIzaSyD1Tg68zZDttDWztxeta06cetEd16O93Yc"]
+    },
+    "kanha": {
+        "url": "https://kanha-3bf53-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "kituu36": {
+        "url": "https://kituu36-58290-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "komaljah": {
+        "url": "https://komaljah-default-rtdb.firebaseio.com",
+        "keys": ["AIzaSyBxSWK2jUvbczs57IgcRTSql_QQ6en_j7A"]
+    },
+    "kumarlive1": {
+        "url": "https://kumarlive1-default-rtdb.firebaseio.com",
+        "keys": [
+            "AIzaSyAD6iCJUEDvl_XFCUZ0VeiGvH571FobXMM",
+            "AIzaSyCbT2cHC05tINJ3aOku1URGlAzWTG1IS1E"
+        ]
+    },
+    "kumu": {
+        "url": "https://kumu-f2257-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "kundan": {
+        "url": "https://kundan-c848c-default-rtdb.firebaseio.com",
+        "keys": ["AIzaSyAwTfmtKb0y6mJ6xne6Nwd6ya5hdfbWci0"]
+    },
+    "lawrence": {
+        "url": "https://lawrence-7b55f-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "ll04": {
+        "url": "https://ll04-210bd-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "loda": {
+        "url": "https://loda-5029e-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "lovefimus": {
+        "url": "https://lovefimus-default-rtdb.firebaseio.com",
+        "keys": [
+            "AIzaSyD2Ry06YV58BdIjbhX5nvdY6MN1IQaRqGk",
+            "AIzaSyCbT2cHC05tINJ3aOku1URGlAzWTG1IS1E"
+        ]
+    },
+    "m05960864": {
+        "url": "https://m05960864-6140e-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "m631835370": {
+        "url": "https://m631835370-cfdc7-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "m74946448": {
+        "url": "https://m74946448-80a23-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "m90910540": {
+        "url": "https://m90910540-a4e00-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "m93374762": {
+        "url": "https://m93374762-8c0f6-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "mahanivip": {
+        "url": "https://mahanivip-kituk10-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "maik": {
+        "url": "https://maik-31440-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "mano99": {
+        "url": "https://mano99-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "manuwa": {
+        "url": "https://manuwa-bb70a-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "master80221": {
+        "url": "https://master80221-753fb-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "master89812": {
+        "url": "https://master89812-b77a6-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "maxa29": {
+        "url": "https://maxa29-f652e-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "mayor": {
+        "url": "https://mayor-6f08c-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "mera-wala": {
+        "url": "https://mera-wala-71a5e-default-rtdb.firebaseio.com",
+        "keys": ["AIzaSyB5caS3wpNz22hBSbR2jCOGoWvOufKDi_o"]
+    },
+    "mera5": {
+        "url": "https://mera5-a7138-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "money": {
+        "url": "https://money-ace2c-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "mpari": {
+        "url": "https://mpari-6a6e5-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "muajob": {
+        "url": "https://muajob-29c86-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "mun4": {
+        "url": "https://mun4-ff5d4-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "myabtar": {
+        "url": "https://myabtar-default-rtdb.firebaseio.com",
+        "keys": [
+            "AIzaSyCbT2cHC05tINJ3aOku1URGlAzWTG1IS1E",
+            "AIzaSyDcUuVf2LFUmZZr4cUVqyjeMT4OiQrWjbg"
+        ]
+    },
+    "myadmin": {
+        "url": "https://myadmin-38635-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "myapp": {
+        "url": "https://myapp-8228a-default-rtdb.firebaseio.com",
+        "keys": ["AIzaSyCbT2cHC05tINJ3aOku1URGlAzWTG1IS1E"]
+    },
+    "navin512": {
+        "url": "https://navin512-54d6f-default-rtdb.firebaseio.com",
+        "keys": ["AIzaSyBiCQmETLwj3ouuJkw7fCkhUtidDVh1x9I"]
+    },
+    "newappi": {
+        "url": "https://newappi-7661a-default-rtdb.firebaseio.com",
+        "keys": ["AIzaSyANVndD8aVFoGpg-w5SbrHPGajrYv5wpzo"]
+    },
+    "newspreding": {
+        "url": "https://newspreding-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "painislv": {
+        "url": "https://painislv-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "panel-v11": {
+        "url": "https://panel-wala-v11-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "panel-v16": {
+        "url": "https://panel-wala-v16-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "panel-v64": {
+        "url": "https://panel-wala-v64-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "panel-v70": {
+        "url": "https://panel-wala-v70-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "panel123628": {
+        "url": "https://panel123628-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "pawankumar": {
+        "url": "https://pawankumar92342038-8f702-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "pehla-green": {
+        "url": "https://pehla-panel-green-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "pinkyrani": {
+        "url": "https://pinkyrani-default-rtdb.firebaseio.com",
+        "keys": ["AIzaSyCOcxmOCivdRSq4w-pYS44g2-P7HdYUyKE"]
+    },
+    "piryankakumari1212c": {
+        "url": "https://piryankakumari1212c-9f29e-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "pk114": {
+        "url": "https://pk114-6e828-default-rtdb.firebaseio.com",
+        "keys": ["AIzaSyBfedy8Szc04wJfMBfYye5p_eQDvN0Nk9A"]
+    },
+    "pm-kisan-20": {
+        "url": "https://pm-kisan-20-vgg-default-rtdb.firebaseio.com",
+        "keys": ["AIzaSyCoZC16SJuhmzNKgBLfGpSW540P7pGsctE"]
+    },
+    "pm-kisan-28": {
+        "url": "https://pm-kisan-28ugg-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "pmfg": {
+        "url": "https://pmfg-ccccc-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "pmnr1newad": {
+        "url": "https://pmnr1newad-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "pmsjdj": {
+        "url": "https://pmsjdj-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "pohn": {
+        "url": "https://pohn-cd7ea-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "pp30": {
+        "url": "https://pp30-fc7e5-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "prof": {
+        "url": "https://prof-b6a64-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "project3": {
+        "url": "https://project3-13fff-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "projectsb0810": {
+        "url": "https://projectsb0810-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "pvn7": {
+        "url": "https://pvn7-a873a-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "r62710898": {
+        "url": "https://r62710898-39a8e-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "rahu80759": {
+        "url": "https://rahu80759-ac69b-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "rahul-54fe9": {
+        "url": "https://rahul-54fe9-default-rtdb.firebaseio.com",
+        "keys": ["AIzaSyB7QRdVFsu_hnKv_-u1RpAAnTVyO4Fz-sM"]
+    },
+    "rahul-6bf55": {
+        "url": "https://rahul-6bf55-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "rahulcsc": {
+        "url": "https://rahulcscperosnl-default-rtdb.firebaseio.com",
+        "keys": [
+            "AIzaSyCbT2cHC05tINJ3aOku1URGlAzWTG1IS1E",
+            "AIzaSyA51lq8IG509h32yHtzaWWWzdyZNqemUkc"
+        ]
+    },
+    "rahulgandhi": {
+        "url": "https://rahulgandhi-d09ca-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "raj-kumar": {
+        "url": "https://raj-kumar-63492-default-rtdb.firebaseio.com",
+        "keys": ["AIzaSyD74TOQXfWjYvfwDQ06U38xhdRUIVJ4Afs"]
+    },
+    "raj254346": {
+        "url": "https://raj254346kumar-84033-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "raja252525": {
+        "url": "https://raja252525raj-4ee9a-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "rajkumar8822556644": {
+        "url": "https://rajkumar8822556644-407f5-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "rajputchuttad": {
+        "url": "https://rajputchuttad-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "raki143aa": {
+        "url": "https://raki143aa-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "rameshwar": {
+        "url": "https://rameshwar-7okt-default-rtdb.firebaseio.com",
+        "keys": []
+    },
+    "randi-rona": {
+        "url": "https://randi-rona-81876-default-rtdb.firebaseio.com",
+        "keys": []
+    }
+}
+
+DEFAULT_TIME = 5400
+REFERRAL_BONUS = 5400
+WEB_PANEL_URL = "https://your-web-panel.com"
+SUPPORT_CONTACT = "@admin"
+GIFT_CARDS = {
+    "GOLD-100": 7200,
+    "SILVER-50": 3600,
+    "BRONZE-25": 1800
+}
